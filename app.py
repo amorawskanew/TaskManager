5857df172e1079645bd4215ef1f14107f42c6529
amorawskanew <amorawska@yahoo.com>
2020-05-04 20:50:41 +0000

    commit

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def add_category():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP',"0.0.0.0"),
+            port=int(os.environ.get('PORT',8080)),
             debug=True)
